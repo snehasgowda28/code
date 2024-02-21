0ef924d985be4d4f06554fc3ef82658dc523a8dc
snehasgowda28 <144375669+snehasgowda28@users.noreply.github.com>
2024-02-21 05:33:12 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
-//pre and post inc and dec
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int a=10,b,c,d;
-    b=++a;
-    c=a--;
-    d=++a;
-    printf("%d,%d,%d,%d",a,b,c,d);
+    int a,b,sum,diff,pro,rem;
+    char op;
+    printf("Enter the operator");
+    scanf("%c",&op);
+    printf("Enter the no");
+    scanf("%d %d",&a,&b);
+
+
+    switch(op)
+    {
+        case '+':
+            sum=a+b;
+            printf("The sum is %d",&sum);
+            break;
+
+        case '-':
+            diff=a-b;
+            printf("The diff is %d",&diff);
+            break;
+
+        case '*':
+            pro=a+b;
+            printf("The product is %d",&pro);
+            break;
+
+        case '/':
+             rem=a+b;
+            printf("The remainder is %d",&rem);
+            break;
+
+        default:
+            printf("invalid operator");
+    }
+
     return 0;
 }
